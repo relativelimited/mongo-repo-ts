46b9d4f455eed0b0e67865479bc077afe1f7cff7
Nick Cousins <me@nickcousins.co.uk>
2018-05-24 23:15:47 +0100

    Types declaration

---
+++ dist/document-incrementer.d.ts
@@ -0,0 +1,6 @@
+import { Db } from "mongodb";
+export default class DocumentIncrementer {
+    db: Db;
+    constructor(db: Db);
+    for(objectClass: string): Promise<string>;
+}
---
+++ dist/repository.d.ts
@@ -0,0 +1,25 @@
+import { FilterQuery } from "mongodb";
+export interface RepositoryInterface<T extends RepositoryDocument> {
+    getByID(id: string): Promise<T>;
+    getAll(): Promise<Array<T>>;
+    save(model: T): Promise<T>;
+    create(model: T): Promise<T>;
+    delete(id: string): Promise<boolean>;
+}
+export interface RepositoryDocument {
+    _id: string;
+    created: string;
+}
+export default class Repository<T extends RepositoryDocument> implements RepositoryInterface<RepositoryDocument> {
+    collectionName: string;
+    modelRef: string;
+    increments: boolean;
+    constructor(collectionName: string, modelRef: string);
+    getByID(id: string): Promise<T>;
+    private static getDb();
+    getAll(query?: FilterQuery<any>): Promise<Array<T>>;
+    save(model: T): Promise<T>;
+    create(model: T): Promise<T>;
+    private getID();
+    delete(id: string): Promise<boolean>;
+}
